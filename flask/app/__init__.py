d9613fd7289beb197f27513fe7f4a08a8620bc20
steve-te <steve-te@gmail.com>
2025-05-31 15:48:37 +0900

    docs: :memo: added swagger

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flasgger import Swagger
 
 def create_app():
     app = Flask(__name__)
+    Swagger(app)  # Swagger 초기화
 
     from .routes import bp as main_bp
     app.register_blueprint(main_bp)
