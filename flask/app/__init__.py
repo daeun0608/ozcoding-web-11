53929597b13ce74d9c28e13565d06b579a3dbca7
steve-te <steve-te@gmail.com>
2025-06-30 19:02:24 +0900

    feat: :sparkles: added test yaml for github action workflows

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flasgger import Swagger
 from .routes import bp as main_bp
 def create_app():
     app = Flask(__name__)
+    Swagger(app)
 
     app.register_blueprint(main_bp)
 
